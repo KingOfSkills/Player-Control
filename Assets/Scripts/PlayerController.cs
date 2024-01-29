1b0d4bbae01bec1c0f4f17adfb65db54d332ff54
Christopher Yang <christopher.yang01@gmail.com>
2024-01-28 20:08:06 -0600

    Added input varible for player to manipulate movement and rotation Made empty parent object for Obstacles Cleaned code and added comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    private float moveSpeed = 15f;
+    private float turnSpeed = 35f;
+    private float horizontalInput;
+    private float forwardInput;
 
     private void Update()
     {
+        // Get Player Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
